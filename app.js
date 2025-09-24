4b07e53db189e2a999537c48ecd1b797b7d75900
Aluna0001 <lusz0001@stud.kea.dk>
2025-09-24 16:29:06 +0200

    setting server up

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+//reads all files from folder "public, including css and html"
+app.use(express.static("public"));
+
+app.get(
+  "/"(requestAnimationFrame, (res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  })
+);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log("Server is running on port:", PORT);
+});
